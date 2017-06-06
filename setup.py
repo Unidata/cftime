4d112fa2eb35bc6ff3f208246c53771859af4852
Filipe <ocefpaf@gmail.com>
2017-06-06 15:52:26 -0300

    Improve Travis-CI and some small packaging improvements (#14)
    * removed ci directory
    
    * simplify travis config and setup.py
    
    * use conda-forge to get coveralls
    
    * import os
    
    * add a manifest file
    
    * ship the source so user can rebuild if they want to

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import os
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup()
+
+rootpath = os.path.abspath(os.path.dirname(__file__))
+
+
+def extract_version(module='netcdftime'):
+    version = None
+    fname = os.path.join(rootpath, module, '_netcdftime.pyx')
+    with open(fname) as f:
+        for line in f:
+            if (line.startswith('__version__')):
+                _, version = line.split('=')
+                version = version.strip()[1:-1]  # Remove quotation characters.
+                break
+    return version
+
+
+with open('requirements.txt') as f:
+    tests_require = f.readlines()
+install_requires = [t.strip() for t in tests_require]
 
 setup(
     name='netcdftime',
@@ -9,7 +28,7 @@ setup(
     author_email='lapok@atmos.washington.edu',
     description='Time-handling functionality from netcdf4-python',
     packages=['netcdftime'],
-    version='0.0',
-    ext_modules=cythonize("netcdftime/*.pyx"),
-    install_requires=['numpy', 'Cython'],
+    version=extract_version(),
+    ext_modules=cythonize('netcdftime/*.pyx'),
+    install_requires=install_requires,
     tests_require=['pytest'])
