eaf536032dd4ffa5e533127d5daee58d4c86bedf
Jeff Whitaker <jswhit@fastmail.fm>
2018-11-09 19:28:26 -0700

    update

--- test/test_cftime.py
+++ test/test_cftime.py
@@ -129,11 +129,10 @@ class cftimeTestCase(unittest.TestCase):
         self.assertTrue(self.cdftime_pg.calendar == 'proleptic_gregorian')
         # check date2num method.
         d = datetime(1990, 5, 5, 2, 17)
-        t1 = np.around(self.cdftime_pg.date2num(d))
-        self.assertTrue(t1 == 62777470620.0)
+        t1 = self.cdftime_pg.date2num(d)
+        self.assertTrue(np.around(t1) == 62777470620.0)
         # check num2date method.
         d2 = self.cdftime_pg.num2date(t1)
-        print(d,d2)
         self.assertTrue(d.strftime(dateformat) == d2.strftime(dateformat))
         # check day of year.
         ndayr = d.timetuple()[7]
