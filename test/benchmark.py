ba3a659326747140c3dcc4a303bc0af57c6c8895
Jeff Whitaker <jswhit@fastmail.fm>
2018-12-02 11:17:06 -0700

    replace time.clock() with time.perf_count() for python 3.8

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,17 +1,20 @@
 from cftime import num2date, date2num
-import time
+try:
+    from time import perf_counter
+except ImportError:
+    from time import clock as perf_counter
 import numpy as np
 units = 'hours since 01-01-01'
 calendar = 'standard'
 timevals = np.arange(0,10000,1)
 print('processing %s values...' % len(timevals))
-t1 = time.clock()
+t1 = perf_counter()
 dates =\
 num2date(timevals,units=units,calendar=calendar,only_use_cftime_datetimes=True)
-t2 = time.clock()
+t2 = perf_counter()
 t = t2-t1
 print('num2date took %s seconds' % t)
 timevals2 = date2num(dates,units=units,calendar=calendar)
-t1 = time.clock()
+t1 = perf_counter()
 t = t1-t2
 print('date2num took %s seconds' % t)
