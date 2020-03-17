a7e0ae48cdf44d9eed1a7720a6a5d3da1fcb31b1
Filipe Fernandes <ocefpaf@gmail.com>
2020-03-17 15:13:52 -0300

    update classifier

--- setup.py
+++ setup.py
@@ -117,8 +117,10 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)']
     )
