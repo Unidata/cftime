cb32c10d53bcc68fc12e97ac90dcbc99e67c044f
jswhit <whitaker.jeffrey@gmail.com>
2020-07-10 10:02:29 -0600

    revised solution

--- test/test_cftime.py
+++ test/test_cftime.py
@@ -111,12 +111,12 @@ class cftimeTestCase(unittest.TestCase):
     def test_tz_aware(self):
         """testing with timezone"""
         self.assertTrue(self.cdftime_mixed.units == 'hours')
-        d1 = datetime(1582, 10, 4, 23, tzinfo=utc)
+        d1 = datetime(1582, 10, 15, 23, tzinfo=utc)
         t1 = self.cdftime_mixed.date2num(d1)
-        d2 = datetime(1582, 10, 4, 18, tzinfo=est)
+        d2 = datetime(1582, 10, 15, 18, tzinfo=est)
         t2 = self.cdftime_mixed.date2num(d2)
-        assert_almost_equal(t1, 13865687.0)
-        assert_almost_equal(t2, 13865682.0) # est is 5 hours behind utc
+        assert_almost_equal(t1, 13865711.0)
+        assert_almost_equal(t2, 13865706.0) # est is 5 hours behind utc
 
     def test_tz_naive(self):
         """testing cftime"""
@@ -843,8 +843,6 @@ class TestDate2index(unittest.TestCase):
     def setUp(self):
         self.standardtime = self.TestTime(datetime(1950, 1, 1), 366, 24,
                                           'hours since 1900-01-01', 'standard')
-        self.prolgregtime = self.TestTime(datetime(1950, 1, 1), 366, 24,
-                                          'hours since 1900-01-01', 'proleptic_gregorian')
 
         self.time_vars = {}
         self.time_vars['time'] = CFTimeVariable(
@@ -875,7 +873,7 @@ class TestDate2index(unittest.TestCase):
         t1 = date2index(dutc, self.standardtime)
         assert_equal(t1, 31)
         dest = datetime(1950, 1, 31, 19, tzinfo=est)
-        t2 = date2index(dest, self.prolgregtime)
+        t2 = date2index(dest, self.standardtime)
         assert_equal(t2, 31)
 
     def test_simple(self):
