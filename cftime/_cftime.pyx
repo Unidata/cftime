b7b501df8ed2b8f2a3b4d4dc1ddd2d88af47365a
Jeff Whitaker <jswhit@fastmail.fm>
2020-07-17 11:12:06 -0600

    fix rounding to nearest second, only do this if units not millisecs/ms

--- cftime/_cftime.pyx
+++ cftime/_cftime.pyx
@@ -356,7 +356,7 @@ _MAX_INT64 = np.iinfo("int64").max
 _MIN_INT64 = np.iinfo("int64").min
 
 
-def cast_to_int(num):
+def cast_to_int(num, units=None):
     if num.dtype.kind in "iu":
         return num
     else:
@@ -366,16 +366,18 @@ def cast_to_int(num):
             int_num = np.ma.masked_array(np.rint(num), dtype=np.int64)
             # use ceil instead of rint if 1 microsec less than a second
             # or floor if 1 microsec greater than a second (issue #187)
-            int_num = np.ma.where(int_num%10000000 == 1, \
-                      np.ma.masked_array(np.floor(num),dtype=np.int64), int_num)
-            int_num = np.ma.where(int_num%10000000 == 9999999, \
-                      np.ma.masked_array(np.ceil(num),dtype=np.int64), int_num)
+            if units not in microsec_units and units not in millisec_units:
+                int_num = np.ma.where(int_num%1000000 == 1, \
+                          np.ma.masked_array(np.floor(num),dtype=np.int64), int_num)
+                int_num = np.ma.where(int_num%1000000 == 999999, \
+                          np.ma.masked_array(np.ceil(num),dtype=np.int64), int_num)
         else:
             int_num = np.array(np.rint(num), dtype=np.int64)
-            int_num = np.where(int_num%10000000 == 1, \
-                      np.array(np.floor(num),dtype=np.int64), int_num)
-            int_num = np.where(int_num%10000000 == 9999999, \
-                      np.array(np.ceil(num),dtype=np.int64), int_num)
+            if units not in microsec_units and units not in millisec_units:
+                int_num = np.where(int_num%1000000 == 1, \
+                          np.array(np.floor(num),dtype=np.int64), int_num)
+                int_num = np.where(int_num%1000000 == 999999, \
+                          np.array(np.ceil(num),dtype=np.int64), int_num)
         return int_num
 
 
@@ -496,7 +498,7 @@ def num2date(
     times = np.asanyarray(times)  # Allow list as input
     times = upcast_times(times)
     scaled_times = scale_times(times, factor)
-    scaled_times = cast_to_int(scaled_times)
+    scaled_times = cast_to_int(scaled_times,units=unit)
 
     # Through np.timedelta64, convert integers scaled to have units of
     # microseconds to datetime.timedelta objects, the timedelta type compatible
