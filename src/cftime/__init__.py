5192cb526e3ba6e48fd2195895586bbb351e2b47
Jeff Whitaker <jswhit@fastmail.fm>
2021-01-31 11:49:07 -0700

    isolate legacy functions

--- src/cftime/__init__.py
+++ src/cftime/__init__.py
@@ -1,8 +1,10 @@
-from ._cftime import utime, JulianDayFromDate, DateFromJulianDay, UNIT_CONVERSION_FACTORS
 from ._cftime import _parse_date, date2index, time2index, datetime, real_datetime
-from ._cftime import DatetimeNoLeap, DatetimeAllLeap, Datetime360Day, DatetimeJulian, \
-                     DatetimeGregorian, DatetimeProlepticGregorian
 from ._cftime import microsec_units, millisec_units, \
-                     sec_units, hr_units, day_units, min_units
+                     sec_units, hr_units, day_units, min_units,\
+                     UNIT_CONVERSION_FACTORS
 from ._cftime import num2date, date2num, date2index, num2pydate
 from ._cftime import __version__
+# legacy functions
+from ._cftime import DatetimeNoLeap, DatetimeAllLeap, Datetime360Day, DatetimeJulian, \
+                     DatetimeGregorian, DatetimeProlepticGregorian
+from ._cftime import utime, JulianDayFromDate, DateFromJulianDay
