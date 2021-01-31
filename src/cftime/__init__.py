479c318eef8d4dc680da862dc2e5193f58ab8900
Jeff Whitaker <jswhit@fastmail.fm>
2021-01-31 12:15:13 -0700

    update

--- src/cftime/__init__.py
+++ src/cftime/__init__.py
@@ -1,10 +1,10 @@
-from ._cftime import _parse_date, date2index, time2index, datetime, real_datetime
+from ._cftime import datetime, real_datetime, _parse_date
+from ._cftime import num2date, date2num, date2index, time2index, num2pydate
 from ._cftime import microsec_units, millisec_units, \
                      sec_units, hr_units, day_units, min_units,\
                      UNIT_CONVERSION_FACTORS
-from ._cftime import num2date, date2num, date2index, num2pydate
 from ._cftime import __version__
-# legacy functions
+# legacy functions in _cftime_legacy.pyx
 from ._cftime import DatetimeNoLeap, DatetimeAllLeap, Datetime360Day, DatetimeJulian, \
                      DatetimeGregorian, DatetimeProlepticGregorian
 from ._cftime import utime, JulianDayFromDate, DateFromJulianDay
