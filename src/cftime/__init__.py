d69ac5899ec4d72852dc340330444917305d1f35
Jeff Whitaker <jswhit@fastmail.fm>
2021-03-15 09:18:29 -0600

    expose CFWarning

--- src/cftime/__init__.py
+++ src/cftime/__init__.py
@@ -3,8 +3,8 @@ from ._cftime import num2date, date2num, date2index, time2index, num2pydate
 from ._cftime import microsec_units, millisec_units, \
                      sec_units, hr_units, day_units, min_units,\
                      UNIT_CONVERSION_FACTORS
-from ._cftime import __version__
-# legacy functions in _cftime_legacy.pyx
+from ._cftime import __version__, CFWarning
 from ._cftime import DatetimeNoLeap, DatetimeAllLeap, Datetime360Day, DatetimeJulian, \
                      DatetimeGregorian, DatetimeProlepticGregorian
+# legacy functions in _cftime_legacy.pyx
 from ._cftime import utime, JulianDayFromDate, DateFromJulianDay
